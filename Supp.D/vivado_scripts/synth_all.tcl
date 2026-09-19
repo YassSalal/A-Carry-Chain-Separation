fdcbb3fc9475f3ec8462661ad3a5ecc8573cb7f8
Yass Khudheir Salal <32617122+YassSalal@users.noreply.github.com>
2026-09-19 15:11:33 +0300

    Update RTL paths and output directory in synth_all.tcl

--- Supp.D/vivado_scripts/synth_all.tcl
+++ Supp.D/vivado_scripts/synth_all.tcl
@@ -4,14 +4,14 @@ set arch   [lindex $argv 0]
 set width  [lindex $argv 1]
 
 set rtl_map [dict create \
-    ccsa               "supp/B_rtl/ccsa_adder.sv" \
-    manchester_ripple  "supp/B_rtl/manchester_adder.sv" \
-    manchester_static  "supp/B_rtl/manchester_adder.sv" \
-    carry_skip         "supp/B_rtl/carry_skip_adder.sv" \
-    ks_pipelined       "supp/B_rtl/kogge_stone_adder.sv" \
-    ks_comb            "supp/B_rtl/kogge_stone_adder.sv" \
-    carry4_ripple      "supp/B_rtl/carry4_adder.sv" \
-    carry4_pipelined   "supp/B_rtl/carry4_adder.sv"]
+    ccsa               "Supp.B/rtl/ccsa_adder.sv" \
+    manchester_ripple  "Supp.B/rtl/manchester_adder.sv" \
+    manchester_static  "Supp.B/rtl/manchester_adder.sv" \
+    carry_skip         "Supp.B/rtl/carry_skip_adder.sv" \
+    ks_pipelined       "Supp.B/rtl/kogge_stone_adder.sv" \
+    ks_comb            "Supp.B/rtl/kogge_stone_adder.sv" \
+    carry4_ripple      "Supp.B/rtl/carry4_adder.sv" \
+    carry4_pipelined   "Supp.B/rtl/carry4_adder.sv"]
 
 set rtl [dict get $rtl_map $arch]
 
@@ -23,7 +23,7 @@ opt_design
 place_design
 route_design
 
-set out supp/F_fpga_reports/raw/${arch}_w${width}
+set out Supp.F/fpga_reports/raw/${arch}_w${width}
 report_timing_summary -file ${out}_timing.rpt
 report_utilization    -file ${out}_util.rpt
 report_power          -file ${out}_power.rpt
